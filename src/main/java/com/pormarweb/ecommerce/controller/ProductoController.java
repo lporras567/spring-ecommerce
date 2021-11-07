7778286b67380eee76dc59a65ce175e7d11ec47e
lporras567 <lporras567@gmail.com>
2021-11-07 11:16:25 -0500

    Creado la funcionalidad para eliminar un registro producto

--- src/main/java/com/pormarweb/ecommerce/controller/ProductoController.java
+++ src/main/java/com/pormarweb/ecommerce/controller/ProductoController.java
@@ -66,4 +66,12 @@ public class ProductoController {
 		return "redirect:/productos";
 	}
 	
+	@GetMapping("/delete/{id}")
+	public String delete(@PathVariable Integer id) {
+		
+		productoService.delete(id);
+		return "redirect:/productos";
+	
+	}
+	
 }
