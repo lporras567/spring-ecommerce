3de9f2fba4b28bf915b257c2dfc93e3f94186e27
lporras567 <lporras567@gmail.com>
2021-11-07 15:11:39 -0500

    Clase de servicio para subir una imagen al servidor

---
+++ src/main/java/com/pormarweb/ecommerce/service/UploadFileService.java
@@ -0,0 +1,34 @@
+package com.pormarweb.ecommerce.service;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class UploadFileService {
+	
+	private String folder = "Images//";
+	
+	public String saveImagen(MultipartFile file) throws IOException {
+		
+		if (!file.isEmpty()) {
+			byte [] bytes=file.getBytes();
+			Path path = Paths.get(folder+file.getOriginalFilename());
+			Files.write(path, bytes);
+			return file.getOriginalFilename();
+		}
+		return "default.jpg";
+		
+	}
+
+	public void deleteImagen(String nombre) {
+		File file=new File(folder+nombre);
+		file.delete();
+	}
+	
+}
