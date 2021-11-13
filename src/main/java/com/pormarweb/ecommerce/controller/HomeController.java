5302e919cd1fe87953d9c170bab2ec59a930a8a6
lporras567 <lporras567@gmail.com>
2021-11-13 11:31:57 -0500

    Redirección al carrito de compras

--- src/main/java/com/pormarweb/ecommerce/controller/HomeController.java
+++ src/main/java/com/pormarweb/ecommerce/controller/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.pormarweb.ecommerce.model.Producto;
@@ -45,6 +46,11 @@ public class HomeController {
 		
 		return "usuario/productohome";
 		
+	}
+	
+	@PostMapping("/cart")
+	public String addCart() {
+		return "usuario/carrito";
 	}
 
 }
