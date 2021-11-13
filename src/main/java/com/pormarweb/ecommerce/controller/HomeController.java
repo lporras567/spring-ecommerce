755bc947ad3e98a3da1df9009b040f6523f41830
lporras567 <lporras567@gmail.com>
2021-11-12 23:56:06 -0500

    Mostrar la vista del producto al usuario

--- src/main/java/com/pormarweb/ecommerce/controller/HomeController.java
+++ src/main/java/com/pormarweb/ecommerce/controller/HomeController.java
@@ -1,17 +1,22 @@
 package com.pormarweb.ecommerce.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.pormarweb.ecommerce.service.ProductoService;
 
 @Controller
 @RequestMapping("/")
-public class HomeController {
+public class HomeController { 
 	
+	private final Logger log = LoggerFactory.getLogger(HomeController.class);
+		
 	@Autowired
 	private ProductoService productoService;
 	
@@ -21,6 +26,14 @@ public class HomeController {
 		model.addAttribute("productos", productoService.findAll());
 		
 		return "usuario/home";
-}
+	}
+	
+	@GetMapping("productohome/{id}")
+	public String productoHome(@PathVariable Integer id) {
+		
+		log.info("id producto enviado como parametro {}", id);
+		return "usuario/productohome";
+		
+	}
 
 }
