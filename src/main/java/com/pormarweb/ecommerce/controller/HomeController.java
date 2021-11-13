93eb48b631f47ee10db809d335a28159803e6d09
lporras567 <lporras567@gmail.com>
2021-11-13 10:42:14 -0500

    Mostrar información del producto en la página productoHome

--- src/main/java/com/pormarweb/ecommerce/controller/HomeController.java
+++ src/main/java/com/pormarweb/ecommerce/controller/HomeController.java
@@ -1,5 +1,8 @@
 package com.pormarweb.ecommerce.controller;
 
+
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.pormarweb.ecommerce.model.Producto;
 import com.pormarweb.ecommerce.service.ProductoService;
 
 @Controller
@@ -29,9 +33,16 @@ public class HomeController {
 	}
 	
 	@GetMapping("productohome/{id}")
-	public String productoHome(@PathVariable Integer id) {
+	public String productoHome(@PathVariable Integer id, Model model) {
 		
 		log.info("id producto enviado como parametro {}", id);
+		
+		Producto producto = new Producto();
+		Optional<Producto> productoOptional = productoService.get(id);
+		producto = productoOptional.get();
+		
+		model.addAttribute("producto", producto);
+		
 		return "usuario/productohome";
 		
 	}
